45668f5cb3542052147f86dfb3f21dafe49f5db4
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-05-20 13:50:58 +0100

    commit

--- routes/api.v1.js
+++ routes/api.v1.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const item_route = require("./items.route");
-const warehouse_route = require("./warehouse.route");
+const warehouse_route = require("./warehouses.route");
 
 router.use("/v1", item_route);
 router.use("/v1", warehouse_route);
